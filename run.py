5d60d03e8933da6dc82c5130fd2fff85d8d87704
Lala Lala Haha HiHi <103634965+RozhBasXYZ@users.noreply.github.com>
2023-07-25 09:12:54 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 import os, RTAG as rozh
-try: os.system("git pull"); os.system("python build.py") ;os.system("python run.py")
+try: os.system("git pull"); os.system("python build.py")
 except: print("[!] anda memakai versi terbaru")
 rozh.main_kan()
